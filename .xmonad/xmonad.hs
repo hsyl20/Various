01f74508cdcb8c076092598a87e31c4a039d8744
Sylvain HENRY <hsyl20@gmail.com>
2014-05-31 13:55:33 +0200

    Minor bépo support

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -14,6 +14,7 @@ import XMonad.Prompt.Window
 import XMonad.Hooks.ManageHelpers
 import XMonad.Layout.NoBorders
 import XMonad.Util.Run
+import qualified XMonad.StackSet as W
 
 import Data.Char (isSpace,isControl)
 import Data.Maybe (fromMaybe, listToMaybe)
@@ -24,7 +25,7 @@ import System.Posix.IO
 import System.Posix.Process (executeFile)
 import Codec.Binary.UTF8.String
 import Control.Monad (unless)
-import qualified Data.Map as M
+import qualified Data.Map as Map
 
 main = do
   myConfigWithBar <- statusBar "xmobar" myPP toggleStrutsKey myConfig
@@ -45,7 +46,7 @@ myConfig = defaultConfig {
 workspaces' = ["1-home","2-mail","3-web","4-dev","5-dev","6-misc", "7-misc","8-misc", "9-music"]
 
 -- Action when a new window is opened
-manageHook'= manageHook defaultConfig <+> manageDocks <+> composeAll [
+manageHook'= manageDocks <+> composeAll [
      isFullscreen --> doFullFloat,
      className =? "Firefox" --> doShift "3-web",
      className =? "Thunderbird" --> doShift "2-mail",
@@ -71,18 +72,18 @@ myPP = defaultPP
               }
 
 -- Aditional key bindings
-keys' x = M.unions [
-      M.fromList $ [
-             ((modMask x .|. shiftMask, xK_Return),   spawn "xterm_pwd"),
-             ((modMask x .|. shiftMask, xK_z),        spawn "slock_hibernate"),
+keys' x = Map.unions [
+      Map.fromList $ [
+             ((modMask x .|. shiftMask, xK_Return),   spawn "/home/hsyl20/.usr/bin/xterm_pwd"),
+             ((modMask x .|. shiftMask, xK_z),        spawn "systemctl hibernate"),
              ((modMask x .|. shiftMask, xK_F12),      spawn "systemctl poweroff"),
-             ((modMask x, xK_F12),                    xmonadPrompt defaultXPConfig),
+             --((modMask x .|. shiftMask, xK_F12),      xmonadPrompt defaultXPConfig),
              ((modMask x, xK_x ),                     shellPrompt defaultXPConfig),
-             ((modMask x, xK_F11 ),                   spawn "slock"),
-             ((modMask x, xK_F4 ),                    sshPrompt defaultXPConfig),
-             ((modMask x, xK_F5 ),                    themePrompt defaultXPConfig),
-             ((modMask x, xK_F6 ),                    windowPromptGoto defaultXPConfig),
-             ((modMask x, xK_F7 ),                    windowPromptBring defaultXPConfig),
+             ((modMask x .|. shiftMask, xK_F11 ),     spawn "slock"),
+             ((modMask x .|. shiftMask, xK_F4 ),      sshPrompt defaultXPConfig),
+             ((modMask x .|. shiftMask, xK_F5 ),      themePrompt defaultXPConfig),
+             ((modMask x .|. shiftMask, xK_F6 ),      windowPromptGoto defaultXPConfig),
+             ((modMask x .|. shiftMask, xK_F7 ),      windowPromptBring defaultXPConfig),
              ((0, xF86XK_AudioLowerVolume),           spawn "amixer set Master 1%-"),
              ((0, xF86XK_AudioRaiseVolume),           spawn "amixer set Master 1%+"),
              ((0, xF86XK_AudioMute),                  spawn "amixer sset Master toggle"),
@@ -90,21 +91,19 @@ keys' x = M.unions [
              ((0, xF86XK_AudioNext),                  spawn "mpc next"),
              ((0, xF86XK_AudioStop),                  spawn "mpc stop"),
              ((0, xF86XK_AudioPlay),                  spawn "mpc toggle"),
-             ((modMask x, xK_F3),                     calcPrompt defaultXPConfig "calc"),
-             ((0, xK_Print),                          spawn "screenshot scr"),
-             ((shiftMask, xK_Print),                  spawn "screenshot win")
-         ],
+             ((0, xK_Print),                          spawn "/home/hsyl20/.usr/bin/screenshot scr"),
+             ((shiftMask, xK_Print),                  spawn "/home/hsyl20/.usr/bin/screenshot win")],
+
+       
+      -- mod-[F1..F9] %! Switch to workspace N
+      -- mod-shift-[F1..F9] %! Move client to workspace N
+      Map.fromList [((m .|. modMask x, k), windows $ f i)
+        | (i, k) <- zip workspaces' [xK_F1 .. xK_F9]
+        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]],
+
       keys defaultConfig x,
       azertyKeys x
    ]
 
 -- Additional mouse bindings
 mouseBindings' = mouseBindings defaultConfig
- 
-calcPrompt :: XPConfig -> String -> X ()
-calcPrompt c ans = inputPrompt c ans ?+ \input -> do
-  output <- liftIO $ runProcessWithInput "ghc" [" -e \"", input, "\""] []
-  calcPrompt c $ filter (not . isControl) $ output
-  where
-    trim  = f . f
-    f = reverse . dropWhile isSpace . dropWhile isControl
