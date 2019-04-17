f5d9294362a47cd1a5d50f52ed5d4f1b27b1cee4
Sylvain Henry <sylvain@haskus.fr>
2019-04-17 12:36:52 +0200

    Disable Intero

--- vim_install.sh
+++ vim_install.sh
@@ -5,16 +5,16 @@ echo "Installing neovim"
 echo "================="
 yay -Sy --needed neovim
 
-echo "================="
-echo "Installing intero"
-echo "================="
-DIR_BUILD_INTERO=~/.cache/build_intero
-if [ -d "$DIR_BUILD_INTERO" ]; then
-   (cd $DIR_BUILD_INTERO ; git fetch ; git reset --hard origin/master)
-else
-   git clone https://github.com/chrisdone/intero $DIR_BUILD_INTERO
-fi
-(cd $DIR_BUILD_INTERO ; stack build)
+# echo "================="
+# echo "Installing intero"
+# echo "================="
+# DIR_BUILD_INTERO=~/.cache/build_intero
+# if [ -d "$DIR_BUILD_INTERO" ]; then
+#    (cd $DIR_BUILD_INTERO ; git fetch ; git reset --hard origin/master)
+# else
+#    git clone https://github.com/chrisdone/intero $DIR_BUILD_INTERO
+# fi
+# (cd $DIR_BUILD_INTERO ; stack build)
 
 echo "==================="
 echo "Installing vim-plug"
